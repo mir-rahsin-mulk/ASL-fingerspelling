eb95738f7f324818c373309df7fa7c7a396b502c
Rahsin <mirmulk@uiowa.edu>
2021-12-09 00:53:39 -0600

    convert to expo camera

--- ASLReactExpo/src/Components/CameraView.js
+++ ASLReactExpo/src/Components/CameraView.js
@@ -1,54 +1,110 @@
-'use strict';
-import React, { PureComponent } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import CameraRoll from "@react-native-community/cameraroll";
-import { RNCamera } from 'react-native-camera';
-import * as tf from '@tensorflow/tfjs';
-import '@tensorflow/tfjs-react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera } from 'expo-camera';
 
-class CameraView extends PureComponent {
-    render() {
-      return (
-        <View style={styles.container}>
-          <RNCamera
-            ref={ref => {
-              this.camera = ref;
-            }}
-            style={styles.preview}
-            type={RNCamera.Constants.Type.front}
-            androidCameraPermissionOptions={{
-              title: 'Permission to use camera',
-              message: 'We need your permission to use your camera',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}
-            androidRecordAudioPermissionOptions={{
-              title: 'Permission to use audio recording',
-              message: 'We need your permission to use your audio',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}
-          />
-          <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-            <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-              <Text style={{ fontSize: 14 }}> SNAP </Text>
+
+import * as tf from '@tensorflow/tfjs';
+import {decodeJpeg} from '@tensorflow/tfjs-react-native';
+
+export default function CameraView () {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const cameraRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  
+  const onSnap = async () => {
+    console.log("snapping")
+    if (cameraRef.current) {
+      console.log("snapping2")
+      const options = { quality: 0.5, base64: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      // const source = data.base64;
+      // CameraRoll.saveToCameraRoll(data.uri);
+      console.log(data.uri);
+      // const fileUri = data.uri;   
+      // const imgB64 = await FileSystem.readAsStringAsync(fileUri, {
+      //   encoding: FileSystem.EncodingType.Base64,
+      // });
+      // const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
+      // const raw = new Uint8Array(imgBuffer)  
+      const imageTensor = decodeJpeg(data.uri);
+
+
+      
+      // // const response = await fetch(data.uri, {}, { isBinary: true });
+      // const imageDataArrayBuffer = data.uri.arrayBuffer();
+      // const imageData = new Uint8Array(imageDataArrayBuffer);
+      // const imageDate = '<some base64 data>';
+      // const imagePath = `${RNFS.TemporaryDirectoryPath}image.jpeg`;
+
+      // RNFS.writeFile(imagePath, source, 'base64')
+      //   .then(() => console.log('Image converted to jpg and saved at ' + imagePath));
+
+      await tf.ready();
+      // tf.image.resize_images(data.uri)
+      // tf.image.resize(
+      //   data.uri, (224, 224), preserve_aspect_ratio=False,
+      // )
+      // const image = require(source)
+      // const imageTensor = decodeJpeg(imageData);
+      // const model = await tf.loadLayersModel('@/Assets/Models/model.json');
+      // const prediction = (await model.predict(imageTensor))[0];
+      // console.log(prediction);
+      this.props.navigateToScore();
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  const onCameraReady = () => {
+    setIsCameraReady(true);
+  };
+  return (
+    <View style={styles.container}>
+      <Camera
+        style={styles.preview}
+        type={type} ratio={"16:9"}
+        ref={cameraRef}
+        onCameraReady={onCameraReady}
+      >
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+            <TouchableOpacity
+              disabled={!isCameraReady}
+              onPress={onSnap} 
+              style={styles.capture}
+            >
+                <Text style={{ fontSize: 14 }}> SNAP </Text>
             </TouchableOpacity>
           </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
         </View>
-      );
-    }
+      </Camera>
+    </View>
+  );
   
-    takePicture = async () => {
-      await tf.ready();
-      console.log('ehere');
-      if (this.camera) {
-        const options = { quality: 0.5, base64: true };
-        const data = await this.camera.takePictureAsync(options);
-        CameraRoll.saveToCameraRoll(data.uri);
-        console.log(data.uri);
-        this.props.navigateToScore()
-      }
-    };
   }
   
   const styles = StyleSheet.create({
@@ -72,5 +128,3 @@ class CameraView extends PureComponent {
       margin: 20,
     },
   });
-
-export default CameraView
